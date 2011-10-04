d48ad4746f071a5b9cf8519b236bc3d337b86c16
Svip <svip@diku.dk>
2011-10-05 00:03:23 +0200

    Så kan man lettere se når man er tilmeldt.

--- includes/pages/Front.php
+++ includes/pages/Front.php
@@ -17,11 +17,11 @@ class Front extends Page {
 				}
 			}
 			if ( $renderSelf )
-				$list .= '<tr><td>'.$this->weekDay($date, true).'</td>
+				$list .= '<tr><td>'.$this->weekDay($date, true).$this->loggedInUserInDate($date).'</td>
 				<td class="date">'.$this->readableDate($date).'</td>
 				<td><a href="?meeting='.$date.'">'.$meeting->{'title'}."</a></td></tr>\n";
 			else
-				$list .= '<tr><td rowspan="'.($uniques+1).'">'.$this->weekDay($date, true).'</td>
+				$list .= '<tr><td rowspan="'.($uniques+1).'">'.$this->weekDay($date, true).$this->loggedInUserInDate($date).'</td>
 				<td class="date" rowspan="'.($uniques+1).'">'.$this->readableDate($date).'</td>
 				<td class="title">'.$meeting->{'title'}."</td></tr>\n";
 			foreach ( $meeting->schedule as $id => $item ) {
@@ -33,8 +33,17 @@ class Front extends Page {
 		$list .= '</table>';
 		$this->content = '<h1>DIKUrevy-møder</h1>';
 		$this->content .= $list;
+		if ( $this->auth->loggedIn() )
+			$this->content .= '<p>* = dage du er tilmeldt.</p>';
 	}
 	
+	private function loggedInUserInDate ( $date ) {
+		if ( !$this->auth->loggedIn() )
+			return '';
+		if ( isset($this->database->getMeeting($date)->users->{$this->auth->userinfo->identity}) )
+			return '*';
+		return '';
+	}
 }
 
 $page = new Front($database, $auth);
