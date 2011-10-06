7e47f8518f07a1b664428c3948681da9f9285d18
Svip <svip@diku.dk>
2011-10-06 17:29:07 +0200

    Fixed issue #7, but not allowed more dates in the future for now.

--- includes/cronjob.py
+++ includes/cronjob.py
@@ -6,9 +6,11 @@ from email.mime.text import MIMEText
 from optparse import OptionParser
 
 class MailTask(object):
+	
+	today = 0
 
 	def monthsInTheFuture ( self, date ):
-		now = datetime.date.today()
+		now = self.today
 		date = date.split('-')
 		date = datetime.date(int(date[0]), int(date[1]), 1)
 		diff = date - now
@@ -21,27 +23,32 @@ class MailTask(object):
 		date = date.split('-')
 		return '%s/%s/%s' % (date[2], date[1], date[0])
 
-	def __init__ ( self ):
+	def __init__ ( self, today=None ):
+		if today != None:
+			s = today.split('-')
+			self.today = datetime.date(int(s[0]), int(s[1]), int(s[2]))
+		else:
+			self.today = datetime.date.today()
 		f = open ( '/var/www/dikurevy/meets/data/meetings.json' )
 		j = json.load(f, 'utf-8')
 		f.close()
 		l = []
 		for date in j:
-			if monthsInTheFuture(date) > 3 or monthsInTheFuture(date) < 0:
+			if self.monthsInTheFuture(date) > 2 or self.monthsInTheFuture(date) < 0:
 				continue
 			l.append([date, j[date]])
-		l.sort(sortMeetings)
+		l.sort(self.sortMeetings)
 		if len(l) < 1:
 			return False
 		s = u'Denne måneds revymøder:'
 		w = False
 		for date in l:
-			if int(date[0].split('-')[1]) <= (datetime.date.today().month + 1):
+			if int(date[0].split('-')[1]) == self.today.month:
 				s = u'''%s
 
 	%s -- %s:
 		%s
-		http://møder.dikurevy.dk/?meeting=%s''' % (s, formatDate(date[0]), date[1]['title'], date[1]['comment'], date[0])
+		http://møder.dikurevy.dk/?meeting=%s''' % (s, self.formatDate(date[0]), date[1]['title'], date[1]['comment'], date[0])
 			else:
 				if not w:
 					s = u'''%s
@@ -50,7 +57,7 @@ class MailTask(object):
 	''' % s
 					w = True
 				s = u'''%s
-	%s -- %s''' % (s, formatDate(date[0]), date[1]['title'])
+	%s -- %s''' % (s, self.formatDate(date[0]), date[1]['title'])
 		s = u'%s\n\nTilmeld dig møder på http://møder.dikurevy.dk/' % s
 		s = s.strip()
 	
@@ -73,10 +80,11 @@ def main ( ):
 	parser = OptionParser()
 	parser.add_option("--daily", dest="daily", action="store_true")
 	parser.add_option("--monthly", dest="monthly", action="store_true")
+	parser.add_option("--date", dest="date", help="Set date to test by.")
 	(options, args) = parser.parse_args()
 	
 	if options.monthly:
-		MailTask()
+		MailTask(options.date)
 	elif options.daily:
 		GitCommit()
 	else:
