0769c11b8671d95c378f912a33bf3ad44ce53c85
Svip <svip@diku.dk>
2011-10-06 11:58:50 +0200

    cronjob update

--- includes/cronjob.py
+++ includes/cronjob.py
@@ -66,7 +66,7 @@ class MailTask(object):
 class GitCommit(object):
 	
 	def __init__( self ):
-		os.system ( "cd /var/www/dikurevy/meets && git commit -a -m 'Data-opdatering'" )
+		os.system ( "cd /var/www/dikurevy/meets && git add data/*.json && git commit -m 'Data-opdatering'" )
 		#&& git push git@github.com:svip/DIKUrevyMeets.git
 
 def main ( ):
