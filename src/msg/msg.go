bd356d9e0cc8a978cd8a4a8022fc90dddecfd474
Svip <svip@diku.dk>
2013-07-09 19:18:47 +0200

    msg.RawMsg() er slet ikke brugt mere, da vi bruger html/template i stedet.

--- src/msg/msg.go
+++ src/msg/msg.go
@@ -1,7 +1,6 @@
 package msg
 
 import (
-	"regexp"
 	"fmt"
 	"encoding/json"
 	"io/ioutil"
@@ -39,31 +38,6 @@ func Msg(msg string, a ...interface{}) string {
 	}
 	return messages[msg]
 	//return RawMsg(messages[msg], a...)
-}
-
-func RawMsg(rawmsg string, a ...interface{}) string {
-	for i, s := range a {
-		expr := fmt.Sprintf("\\$%d([^0-9]|$)", i+1)
-		r, err := regexp.Compile(expr)
-		if err != nil {
-			return ""
-		}
-		var repl string
-		switch s.(type) {
-			case int:
-				repl = fmt.Sprintf("%d", s)
-			case float64:
-			case float32:
-				repl = fmt.Sprintf("%f", s)
-			case string:
-				repl = fmt.Sprintf("%s", s)
-			//case []byte:
-			//	n, err := conn.Read(s[0:])
-			//	repl = string(s[0:n])
-		}
-		rawmsg = r.ReplaceAllString(rawmsg, repl)
-	}
-	return rawmsg
 }
 
 func HtmlMsg(existingContent string, tmplText string, data interface{}) (string, error) {
